b8dd92d419fd63634f2499dde2788f8f001f004d
rohithraj095@gmail.com <rohithraj095@gmail.com>
2021-03-21 21:43:54 -0500

    added full date and time functionality

--- src/screens/ShowReviews.js
+++ src/screens/ShowReviews.js
@@ -48,14 +48,41 @@ const types = [
     <Icon icon={BackIcon}/>
   );
 
-const Header = ({props, title, user, date, rate}) => (
+  const showTime = ({date, datetime}) => {
+    let diff = Math.floor((datetime - date)/1000);
+    let ret = '';
+
+    if (diff < 3600) {
+      ret = Math.floor(diff/60) + ' mins';
+    } else if (diff < 24*3600) {
+      ret = Math.floor(diff/(3600)) + ' hrs';
+    } else if (diff < 365*24*3600) {
+      ret = Math.floor(diff/(24*3600)) + ' days';
+    } else {
+      ret = Math.floor(diff/(365*24*3600)) + ' yrs';
+    }
+    return ret;
+  }
+
+const Header = ({props, title, user, date, rate, edited, edited_time}) => {
+  let dateString = "";
+  const today = new Date();
+  const datetime = today.getTime();
+  if (edited) {
+    dateString = showTime(date, datetime) + " (edited: "+ showTime(edited_time, datetime) + ")";
+  }
+  else {
+    dateString = showTime(date, datetime);
+  }
+  return (
     <View {...props} style={[styles.headerContainer]}>
-        <Text category='h6'> Topic: {title} </Text>
-        <Text category='s1'> User: {user} </Text>
-        <Text category='s3'> Rating(#/10): {rate} </Text>
-        {/* <Text category='h9'> Date: {date} </Text> */}
+      <Text category='h6'> Topic: {title} </Text>
+      <Text category='s1'> User: {user} </Text>
+      <Text category='s3'> Rating(#/10): {rate} </Text>
+      <Text category='h9'> {dateString} </Text>
     </View>
-);
+  )
+};
 
 const Footer = ({navigation, props, title, user, rate, text, review_id, index, currentUser}) => {
     return user == currentUser ? (
@@ -115,7 +142,7 @@ export default showReviews = ({navigation, route }) => {
     const screenWidth = Dimensions.get('window').width;
     const screenHeight = Dimensions.get('window').height;
     let reviews = [];
-    let fields = [];
+    let reviewIDs = [];
 
     Firebase.database()
     .ref(types[index] + ' Reviews/')
@@ -124,8 +151,9 @@ export default showReviews = ({navigation, route }) => {
       console.log(snapshot);
       snapshot.forEach(function (data) {
         console.log('data');
+        reviews.push(data.toJSON());
         console.log(data);
-        reviews.push(data.key);
+        reviewIDs.push(data.key);
       });
     });
 
@@ -183,38 +211,29 @@ export default showReviews = ({navigation, route }) => {
                         
                     </Button>
                     <React.Fragment>
-                        {reviews.map(function(review_id, i) {
-                                Firebase.database().ref(types[index] + ' Reviews/' + review_id).on('value', (snapshot) => {
-                                    console.log(types[index] + ' Reviews/' + review_id);
-                                    let i = 0;
-                                    snapshot.forEach(function(data) {
-                                        fields.push(data);
-                                        // console.log(data);
-                                        i++;
-                                    });
-                                    // console.log(fields[0]);
-                                    
-                                });
+                        {reviews.map(function(review, i) {
 
-                                let title = JSON.parse(JSON.stringify(fields[5 * i + 3]));
-                                let user = JSON.parse(JSON.stringify(fields[5 * i + 4]));
-                                let date = JSON.parse(JSON.stringify(fields[5 * i]));
-                                let rate = JSON.parse(JSON.stringify(fields[5 * i + 1]));
-                                let text = JSON.parse(JSON.stringify(fields[5 * i + 2]));
+                          let title = review.review_title;
+                          let user = review.user;
+                          let date = review.date_time;
+                          let rate = review.review_rate;
+                          let text = review.review_text;
+                          let edited = review.edited;
+                          let edited_time = review.edited_time;
                                 
                                 return  (
                                     <Layout style={styles.container} level={'1'} > 
                                         <TouchableOpacity>
                                             <Card style={styles.card}
-                                            header={(props) => <Header {...props} title={title} user={user} date={date} rate={rate}/> }
-                                            footer={(props) => <Footer {...props} title={title} user={user} rate={rate} text={text} review_id={review_id} navigation={navigation} index={index} currentUser={currentUser}/>}
+                                            header={(props) => <Header {...props} title={title} user={user} date={date} rate={rate} edited={edited} edited_time={edited_time}/> }
+                                            footer={(props) => <Footer {...props} title={title} user={user} rate={rate} text={text} review_id={reviewIDs[i]} navigation={navigation} index={index} currentUser={currentUser}/>}
                                             onPress={() => {
                                                 navigation.navigate('ReadReview', {
                                                   title: title,
                                                   user: user,
                                                   rate: rate,
                                                   text: text,
-                                                  review_id: review_id,
+                                                  review_id: reviewIDs[i],
                                                   date: date,
                                                   index: index
                                                 });
