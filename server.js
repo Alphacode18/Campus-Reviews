2dcf2c7e78ff1c26b931be8795a2f98928733416
Shreyas Kharbanda <skharban@purdue.edu>
2021-03-31 19:42:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.get('/:id', (req, res, next) => {
 });
 
 async function run(url, email) {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    'args' : [
+    '--no-sandbox',
+    '--disable-setuid-sandbox']
+  });
   const page = await browser.newPage();
   await page.goto(url);
   await page.type('input[name=SearchString]', email);
